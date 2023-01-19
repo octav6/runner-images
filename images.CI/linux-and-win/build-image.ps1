5fe5718fba3bbce81481fbc0c0d4daab7bb7d5cd
SheGe <szymon.sito@hotmail.com>
2023-01-19 16:04:33 +0100

    Allow to use static resource group in Azure for Linux and Windows image variants

--- images.CI/linux-and-win/build-image.ps1
+++ images.CI/linux-and-win/build-image.ps1
@@ -1,16 +1,17 @@
 param(
-    [String] [Parameter (Mandatory=$true)] $TemplatePath,
-    [String] [Parameter (Mandatory=$true)] $ClientId,
-    [String] [Parameter (Mandatory=$true)] $ClientSecret,
-    [String] [Parameter (Mandatory=$true)] $ResourcesNamePrefix,
-    [String] [Parameter (Mandatory=$true)] $Location,
-    [String] [Parameter (Mandatory=$true)] $ResourceGroup,
-    [String] [Parameter (Mandatory=$true)] $StorageAccount,
-    [String] [Parameter (Mandatory=$true)] $SubscriptionId,
-    [String] [Parameter (Mandatory=$true)] $TenantId,
-    [String] [Parameter (Mandatory=$false)] $VirtualNetworkName,
-    [String] [Parameter (Mandatory=$false)] $VirtualNetworkRG,
-    [String] [Parameter (Mandatory=$false)] $VirtualNetworkSubnet
+    [String] [Parameter (Mandatory = $true)] $TemplatePath,
+    [String] [Parameter (Mandatory = $true)] $ClientId,
+    [String] [Parameter (Mandatory = $true)] $ClientSecret,
+    [String] [Parameter (Mandatory = $true)] $ResourcesNamePrefix,
+    [String] [Parameter (Mandatory = $true)] $Location,
+    [String] [Parameter (Mandatory = $true)] $ResourceGroup,
+    [String] [Parameter (Mandatory = $true)] $StorageAccount,
+    [String] [Parameter (Mandatory = $true)] $SubscriptionId,
+    [String] [Parameter (Mandatory = $true)] $TenantId,
+    [String] [Parameter (Mandatory = $false)] $VirtualNetworkName,
+    [String] [Parameter (Mandatory = $false)] $VirtualNetworkRG,
+    [String] [Parameter (Mandatory = $false)] $VirtualNetworkSubnet,
+    [String] [Parameter (Mandatory = $false)] $BuildResourceGroupName
 )
 
 if (-not (Test-Path $TemplatePath))
@@ -21,6 +22,12 @@ if (-not (Test-Path $TemplatePath))
 
 $Image = [io.path]::GetFileName($TemplatePath).Split(".")[0]
 $TempResourceGroupName = "${ResourcesNamePrefix}_${Image}"
+
+if (-not [string]::IsNullOrEmpty($BuildResourceGroupName))
+{
+    $TempResourceGroupName = [string]::Empty
+}
+
 $InstallPassword = [System.GUID]::NewGuid().ToString().ToUpper()
 
 packer validate -syntax-only $TemplatePath
@@ -40,24 +47,25 @@ packer --version
 
 Write-Host "Build $Image VM"
 packer build    -var "capture_name_prefix=$ResourcesNamePrefix" `
-                -var "client_id=$ClientId" `
-                -var "client_secret=$ClientSecret" `
-                -var "install_password=$InstallPassword" `
-                -var "location=$Location" `
-                -var "resource_group=$ResourceGroup" `
-                -var "storage_account=$StorageAccount" `
-                -var "subscription_id=$SubscriptionId" `
-                -var "temp_resource_group_name=$TempResourceGroupName" `
-                -var "tenant_id=$TenantId" `
-                -var "virtual_network_name=$VirtualNetworkName" `
-                -var "virtual_network_resource_group_name=$VirtualNetworkRG" `
-                -var "virtual_network_subnet_name=$VirtualNetworkSubnet" `
-                -var "run_validation_diskspace=$env:RUN_VALIDATION_FLAG" `
-                -color=false `
-                $TemplatePath `
-        | Where-Object {
-            #Filter sensitive data from Packer logs
-            $currentString = $_
-            $sensitiveString = $SensitiveData | Where-Object { $currentString -match $_ }
-            $sensitiveString -eq $null
-        }
+    -var "client_id=$ClientId" `
+    -var "client_secret=$ClientSecret" `
+    -var "install_password=$InstallPassword" `
+    -var "location=$Location" `
+    -var "resource_group=$ResourceGroup" `
+    -var "storage_account=$StorageAccount" `
+    -var "subscription_id=$SubscriptionId" `
+    -var "temp_resource_group_name=$TempResourceGroupName" `
+    -var "build_resource_group_name=$BuildResourceGroupName" `
+    -var "tenant_id=$TenantId" `
+    -var "virtual_network_name=$VirtualNetworkName" `
+    -var "virtual_network_resource_group_name=$VirtualNetworkRG" `
+    -var "virtual_network_subnet_name=$VirtualNetworkSubnet" `
+    -var "run_validation_diskspace=$env:RUN_VALIDATION_FLAG" `
+    -color=false `
+    $TemplatePath `
+| Where-Object {
+    #Filter sensitive data from Packer logs
+    $currentString = $_
+    $sensitiveString = $SensitiveData | Where-Object { $currentString -match $_ }
+    $sensitiveString -eq $null
+}
