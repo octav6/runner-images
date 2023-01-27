19bc59351290f9ded8807cce228a87a25ae1baad
octavian tanasov <octavian.tanasov@fenergo.com>
2023-01-27 09:09:19 +0000

    script and pipeline updates

--- images.CI/linux-and-win/build-image.ps1
+++ images.CI/linux-and-win/build-image.ps1
@@ -28,10 +28,7 @@ if (-not [string]::IsNullOrEmpty($BuildResourceGroupName))
     $TempResourceGroupName = [string]::Empty
     $Location = [string]::Empty
 }
-elseif ($BuildResourceGroupName -eq "")
-{
-    [string]::IsNullOrEmpty($BuildResourceGroupName)
-}
+
 $InstallPassword = [System.GUID]::NewGuid().ToString().ToUpper()
 
 packer validate -syntax-only $TemplatePath
